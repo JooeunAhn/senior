4ebb59b6c897d1beb09c2ec20df8ce7f1bdc95d6
frmh0313 <rmh0313@gmail.com>
2016-02-11 20:41:19 +0900

    implementing searching

--- senior/blog/views.py
+++ senior/blog/views.py
@@ -9,27 +9,26 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.http import Http404, HttpResponseForbidden
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
 import re
-from hitcount.views import HitCountMixin
-from hitcount.models import HitCount
-from hitcount.views import HitCountDetailView
+
 # Create your views here.
 reg_b = re.compile(r"(android|bb\\d+|meego).+mobile|avantgo|bada\\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|wap|windows ce|xda|xiino", re.I|re.M)
 reg_v = re.compile(r"1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\\-(n|u)|c55\\/|capi|ccwa|cdm\\-|cell|chtm|cldc|cmd\\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\\-s|devi|dica|dmob|do(c|p)o|ds(12|\\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\\-|_)|g1 u|g560|gene|gf\\-5|g\\-mo|go(\\.w|od)|gr(ad|un)|haie|hcit|hd\\-(m|p|t)|hei\\-|hi(pt|ta)|hp( i|ip)|hs\\-c|ht(c(\\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\\-(20|go|ma)|i230|iac( |\\-|\\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\\/)|klon|kpt |kwc\\-|kyo(c|k)|le(no|xi)|lg( g|\\/(k|l|u)|50|54|\\-[a-w])|libw|lynx|m1\\-w|m3ga|m50\\/|ma(te|ui|xo)|mc(01|21|ca)|m\\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\\-2|po(ck|rt|se)|prox|psio|pt\\-g|qa\\-a|qc(07|12|21|32|60|\\-[2-7]|i\\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\\-|oo|p\\-)|sdk\\/|se(c(\\-|0|1)|47|mc|nd|ri)|sgh\\-|shar|sie(\\-|m)|sk\\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\\-|v\\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\\-|tdg\\-|tel(i|m)|tim\\-|t\\-mo|to(pl|sh)|ts(70|m\\-|m3|m5)|tx\\-9|up(\\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\\-|your|zeto|zte\\-", re.I|re.M)
 
 
-
+# is it ok to do this in views.py?
 
 def mobiles(request):
     mobileuser = True
-    user_agent = request.META.get('HTTP_USER_AGENT','').lower()
+    user_agent = request.META.get('HTTP_USER_AGENT', '').lower()
     b = reg_b.search(user_agent)
     v = reg_v.search(user_agent[0:4])
     if b or v:
         mobileuser = True
     else:
         mobileuser = False
-    return render(request, 'blog/index.html', {'mobileuser':mobileuser})
+    return render(request, 'blog/index.html', {'mobileuser': mobileuser})
 
 def owner_required_freeboard(model_cls, user_field_name = 'author'):
 
@@ -53,25 +52,26 @@ def owner_required_freeboard(model_cls, user_field_name = 'author'):
         return inner_wrap
     return wrap
 
-"""
-실패!
-def hit_count(hitcount=1,):
-    def wrap_fn(view_fn):
-        def wrap(request, *args, **kwargs):
-            HitCountMixin.hit_count(request, hitcount)
-            return view_fn(request, *args, **kwargs)
-        return wrap
-    return wrap_fn
-"""
 
 def index(request):
     return render(request, 'blog/index.html')
 
 
 def mentor_list(request):
-    mentor_list = Profile.objects.filter(is_mentor = True)
+    mentor_list = Profile.objects.filter(is_mentor=True)
+    query_mentor = request.GET.get('mentor')
+
+    if query_mentor:
+        mentor_list = mentor_list.filter(
+            Q(category__title__contains=query_mentor) |
+            Q(self_intro__contains=query_mentor) |
+            Q(user__username__contains=query_mentor)).distinct()
+    else:
+        pass
+
     paginator = Paginator(mentor_list, 10)
     page = request.GET.get('page')
+
     try:
         mentor_list = paginator.page(page)
     except PageNotAnInteger:
@@ -111,8 +111,20 @@ def question_list(request):
     if user.is_mentor :
         questions = Question.objects.filter(mentor = user)
         return render(request, 'blog/question_list.html', {'question_list' : questions})
-    else :
+    else:
         questions = Question.objects.filter(mentee = user)
+
+    query_question = request.GET.get('question')
+
+    if query_question:
+        questions = questions.filter(
+            Q(mentor__username__contains=query_question) |
+            Q(mentee__username__contains=query_question) |
+            Q(title__contains=query_mentor) |
+            Q(message__contains=query_mentor)).distinct()
+    else:
+        pass 
+
         return render(request, 'blog/question_list.html', {'question_list': questions})
 
 @login_required
@@ -138,8 +150,6 @@ def question_delete(request, pk):
             messages.success(request, '삭제완료')
             return redirect("blog:question_list")
         return render(request, 'blog/question_confirm_delete.html', {'quesiton' : question ,})
-
-
 
 
 #def review_list (request, mentor_pk):
@@ -205,6 +215,17 @@ def review_delete(request, mentor_pk, pk):
 def notice(request):
     notice_list = Notice.objects.all()
     paginator = Paginator(notice_list, 10)
+
+    query_notice = request.GET.get('notice')
+
+    if query_notice:
+        notice_list = notice_list.filter(
+            Q(category__title__contains=query_notice) |
+            Q(title__contains=query_notice) |
+            Q(content__contains=query_notice)).distinct()
+    else:
+        pass
+
     page = request.GET.get('page')
     try:
         notice = paginator.page(page)
@@ -231,29 +252,16 @@ def notice_new(request):
         return redirect('blog:index')
 
 
-class FreeboardDetailView(HitCountDetailView):
-    model = Freeboard
-    template_name = 'blog/freeboard_detail.html'
-    count_hit = True
+class NoticeDetailView(DetailView):
+    def get_object(self, queryset=None):
+        try:
+            return Notice.objects.get(pk=self.kwargs['pk'])
+        except Notice.DoesNotExist:
+            raise Http404
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(FreeboardDetailView, self).get_context_data(*args, **kwargs)
-        context['comment_form'] = CommentForm()
-        return context
+        return get_object_or_404(Notice, pk=pk)
 
-# freeboard_detail = DetailView.as_view(model=Freeboard, template_name='blog/freeboard_detail.html')
-freeboard_detail = FreeboardDetailView.as_view()
-
-class NoticeDetailView(HitCountDetailView):
-    model = Notice
-    template_name = 'blog/notice_detail.html'
-    count_hit = True
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(NoticeDetailView, self).get_context_data(*args, **kwargs)
-        return context
-
-notice_detail = NoticeDetailView.as_view()
+notice_detail = NoticeDetailView.as_view(model=Notice)
 
 
 @login_required
@@ -317,28 +325,12 @@ def freeboard_edit(request, pk):
     return render(request, 'blog/freeboard_form.html', {'form':form})
 
 
-'''
 def freeboard_detail(request, pk):
     freeboard = get_object_or_404(Freeboard, pk=pk)
     return render(request, 'blog/freeboard_detail.html', {
         'freeboard': freeboard,
         'comment_form': CommentForm(),
-        # 'hitcount' : hitcount,
      })
-'''
-
-class FreeboardDetailView(HitCountDetailView):
-    model = Freeboard
-    template_name = 'blog/freeboard_detail.html'
-    count_hit = True
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(FreeboardDetailView, self).get_context_data(*args, **kwargs)
-        context['comment_form'] = CommentForm()
-        return context
-
-# freeboard_detail = DetailView.as_view(model=Freeboard, template_name='blog/freeboard_detail.html')
-freeboard_detail = FreeboardDetailView.as_view()
 
 """
 김지은 detailview
@@ -375,6 +367,7 @@ def comment_new(request, freeboard_pk):
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit = False)
+            comment.freeboard = freeboard
             comment.author = Profile.objects.get(user=request.user)
             comment.save()
             return redirect('blog:freeboard_detail', freeboard_pk)
