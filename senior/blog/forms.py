dcc7ced938e16597c88c7bdad3efe75b80e98f29
Woojae-Shin <woojae9308@snu.ac.kr>
2016-02-11 23:56:45 +0900

    자유게시판 질문

--- senior/blog/forms.py
+++ senior/blog/forms.py
@@ -30,6 +30,9 @@ class CommentForm(forms.ModelForm):
   class Meta:
     model = Comment
     fields = ['message']
+    widgets = {
+      'message':forms.Textarea(attrs = {'style' : 'width : 100%; max_height : 20%', }),
+    }
 
 
 class ReplyForm(forms.ModelForm):
